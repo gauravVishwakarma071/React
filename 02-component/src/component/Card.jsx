dd17da7512499acb2c740f18db57397c0efe7f97
Gaurav <gauravvishwakarma071@gmail.com>
2025-12-30 14:20:58 +0530

    Crad view

---
+++ 02-component/src/component/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+
+  const user = 'Gaurav';  
+  
+  return (
+  <div>
+      <div className='card'>
+        <h1>{user} Vishwakarma</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatibus nostrum dolores.</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Card
