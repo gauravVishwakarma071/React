57999360e550352b1ddd8dabb11f4ca29ee880a2
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-02 15:05:50 +0530

    props handling function(CARD).

--- 03-pros/src/components/Card.jsx
+++ 03-pros/src/components/Card.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
     return (
-
-
-        <div className='card'>
-            <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image" className='img' />
-            <h1>Gaurav Vishwakarma</h1>
+            <div className='card'>
+            <img src={props.img} alt="image" className='img' />
+            <h1>{props.name}, {props.age}</h1>
             <p>An MCA student passionate about IT, Computer Science, and Mathematics.
             </p>
-            <button>View Profile</button>
+            <button className='btn'>View Profile</button>
         </div>
-
     )
 }
 
