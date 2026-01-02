398d4aa29d8d1f6c884787ff1259e6de4165d1f0
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-02 13:20:51 +0530

    02-components

--- 02-component/src/component/Card.jsx
+++ 02-component/src/component/Card.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const Card = () => {
 
   const user = 'Gaurav';  
+  const age = 23;  
   
   return (
   <div>
       <div className='card'>
         <h1>{user} Vishwakarma</h1>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatibus nostrum dolores.</h2>
+        <h2> I am {age} years old, Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatibus nostrum dolores.</h2>
       </div>
     </div>
   )
---
+++ 02-component/src/component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+        <h1>This is Navigation Bar</h1>
+    </div>
+  )
+}
+
+export default Navbar
