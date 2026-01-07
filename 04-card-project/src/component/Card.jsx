bb4ac666bbb8516f9aa4d2fc05eb2e08e2423758
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-07 16:20:52 +0530

    card design

---
+++ 04-card-project/src/component/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Bookmark} from 'lucide-react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <div className="top">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv48DgacV_072bfO_oyhmMXGnNvIgQwWoH5Q&s" alt="canpany-image"/>
+        <button>Save <Bookmark /></button>
+      </div>
+      <div className="center">
+        <h3>Amazon <span>5 days ago</span></h3>
+        <h2>Senior UI/UX Designer</h2>
+        <div>
+            <h4>Full time</h4>
+            <h4>Senior level</h4>
+        </div>
+      </div>
+      <div className="bottom">
+        <div>
+            <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai</p>
+            </div>
+            <button>Apply now</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
