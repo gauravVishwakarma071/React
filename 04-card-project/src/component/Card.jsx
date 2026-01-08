3d4fcf8377eb7a789659f340ff7895e1be3b5f81
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-08 14:40:10 +0530

    card view fial design

--- 04-card-project/src/component/Card.jsx
+++ 04-card-project/src/component/Card.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
-import {Bookmark} from 'lucide-react'
+import { Bookmark } from 'lucide-react'
 
 const Card = () => {
-  return (
-    <div className='card'>
-      <div className="top">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv48DgacV_072bfO_oyhmMXGnNvIgQwWoH5Q&s" alt="canpany-image"/>
-        <button>Save <Bookmark /></button>
-      </div>
-      <div className="center">
-        <h3>Amazon <span>5 days ago</span></h3>
-        <h2>Senior UI/UX Designer</h2>
-        <div>
-            <h4>Full time</h4>
-            <h4>Senior level</h4>
-        </div>
-      </div>
-      <div className="bottom">
-        <div>
+    return (
+        <div className='card'>
             <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai</p>
+                <div className="top">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv48DgacV_072bfO_oyhmMXGnNvIgQwWoH5Q&s" alt="canpany-image" />
+                <button>Save <Bookmark size={12} /></button>
             </div>
-            <button>Apply now</button>
+            <div className="center">
+                <h3>Amazon <span>5 days ago</span></h3>
+                <h2>Senior UI/UX Designer</h2>
+                <div className='tag'>
+                    <h4>Full time</h4>
+                    <h4>Senior level</h4>
+                </div>
+            </div>
+            </div>
+            <div className="bottom">
+                <div>
+                    <h3>$120/hr</h3>
+                    <p>Mumbai</p>
+                </div>
+                <button>Apply now</button>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Card
