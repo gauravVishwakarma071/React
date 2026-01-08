9173be06d61437f69ea841dcb3e22baf5cd6ba08
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-08 14:46:34 +0530

    passing data {arr.map(function(elem){ })}

--- 04-card-project/src/App.jsx
+++ 04-card-project/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Card from './component/Card'
+import User from './component/User'
 
 const App = () => {
+
+
+  const arr = ['gaurav','harsh','sarthak']
+
   return (
     <div className='parent'>
-      <Card/>
+     {arr.map(function(elem){
+      return <h1>{elem}</h1>
+     })}
     </div>
   )
 }
