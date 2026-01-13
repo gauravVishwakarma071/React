6344b01bcb411a73a0ed1f143f709af057153384
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-13 16:16:58 +0530

    passing arr

--- 04-card-project/src/App.jsx
+++ 04-card-project/src/App.jsx
@@ -5,12 +5,25 @@ import User from './component/User'
 const App = () => {
 
 
-  const arr = ['gaurav','harsh','sarthak']
+  const arr = [
+    {
+      user:'gaurav',
+      age: 23
+    },
+    {
+      user:'harsh',
+      age: 29
+    },
+    {
+      user:'sarthak',
+      age: 25
+    }
+  ];
 
   return (
     <div className='parent'>
      {arr.map(function(elem){
-      return <h1>{elem}</h1>
+      console.log(elem)
      })}
     </div>
   )
