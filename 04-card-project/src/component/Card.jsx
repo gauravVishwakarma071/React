8e88873ce06b82967979c7883eb38c4994945738
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-19 14:51:54 +0530

    project-01 Done

--- 04-card-project/src/component/Card.jsx
+++ 04-card-project/src/component/Card.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { Bookmark } from 'lucide-react'
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='card'>
             <div>
                 <div className="top">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv48DgacV_072bfO_oyhmMXGnNvIgQwWoH5Q&s" alt="canpany-image" />
-                <button>Save <Bookmark size={12} /></button>
-            </div>
-            <div className="center">
-                <h3>Amazon <span>5 days ago</span></h3>
-                <h2>Senior UI/UX Designer</h2>
-                <div className='tag'>
-                    <h4>Full time</h4>
-                    <h4>Senior level</h4>
+                    <img src={props.logo} />
+                    <button>Save <Bookmark size={12} /></button>
                 </div>
-            </div>
+                <div className="center">
+                    <h3>{props.name} <span>{props.date}</span></h3>
+                    <h2>{props.desig}</h2>
+                    <div className='tag'>
+                        <h4>{props.tag1}</h4>
+                        <h4>{props.tag2}</h4>
+                    </div>
+                </div>
             </div>
             <div className="bottom">
                 <div>
-                    <h3>$120/hr</h3>
-                    <p>Mumbai</p>
+                    <h3>{props.pay}</h3>
+                    <p>{props.location}</p>
                 </div>
                 <button>Apply now</button>
             </div>
