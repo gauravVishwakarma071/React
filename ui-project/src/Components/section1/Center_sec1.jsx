ebe8646c8cfdc1b06e830f3db5c8f26c85f92e8f
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-23 23:11:32 +0530

    g55

--- ui-project/src/Components/section1/Center_sec1.jsx
+++ ui-project/src/Components/section1/Center_sec1.jsx
@@ -4,7 +4,7 @@ import RightContent from './RightCont'
 
 const Center_sec1 = () => {
   return (
-    <div className='h-[90vh] py-10 px-18 flex items-center justify-between bg-amber-900 gap-10'>
+    <div className='h-[90vh] py-10 px-18 flex items-center justify-between bg-amber-900 gap-10 '>
       <LeftContent/>
       <RightContent/>
     </div>
--- ui-project/src/Components/section1/LeftCont.jsx
+++ ui-project/src/Components/section1/LeftCont.jsx
@@ -4,7 +4,7 @@ import 'remixicon/fonts/remixicon.css'
 const leftContent = () => {
   return (
     <div className='h-full w-3/11 bg-amber-200 justify-between'>
-      <div className='text-5xl'>
+      <div className='text-5xl font-bold'>
         <h3>Prospective <br /> <span>Customer</span> <br /> Segmentation</h3>
       </div>
       <div className='text-8xl'>
