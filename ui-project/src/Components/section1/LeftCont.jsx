39f0e225b7ab395428b52f3c9db14ab209068257
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-23 23:23:31 +0530

    center->Left content done

--- ui-project/src/Components/section1/LeftCont.jsx
+++ ui-project/src/Components/section1/LeftCont.jsx
@@ -3,11 +3,12 @@ import 'remixicon/fonts/remixicon.css'
 
 const leftContent = () => {
   return (
-    <div className='h-full w-3/11 bg-amber-200 justify-between'>
-      <div className='text-5xl font-bold'>
-        <h3>Prospective <br /> <span>Customer</span> <br /> Segmentation</h3>
+    <div className='h-full w-3/11 justify-between flex flex-col justify-between'>
+      <div className='p-5'>
+        <h3 className='mb-7 text-5xl font-bold'>Prospective <br /> <span>Customer</span> <br /> Segmentation</h3>
+        <p className='text-xl font-medium text-gray-700'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque rerum eligendi ducimus nostrum est tempora, voluptates voluptate maiores quae quas?</p>
       </div>
-      <div className='text-8xl'>
+      <div className='text-9xl'>
         <i className="ri-arrow-right-up-line"></i>
       </div>
     </div>
