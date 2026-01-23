2436d5503c591ab816a89cfb1b7df7c918bc5a21
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-23 23:10:27 +0530

    run.....

--- ui-project/src/Components/section1/LeftCont.jsx
+++ ui-project/src/Components/section1/LeftCont.jsx
@@ -3,11 +3,11 @@ import 'remixicon/fonts/remixicon.css'
 
 const leftContent = () => {
   return (
-    <div className='h-full w-3/7 bg-amber-300 justify-between'>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse inventore tempore magnam iusto quia dignissimos nihil quaerat ea fuga expedita. Accusantium facilis, eius sunt quis voluptates cum velit iure repudiandae. Ut, assumenda! Ab minus quas ratione quod quis maxime tempore, reiciendis fugit facilis, necessitatibus molestias quo in mollitia itaque ex!
+    <div className='h-full w-3/11 bg-amber-200 justify-between'>
+      <div className='text-5xl'>
+        <h3>Prospective <br /> <span>Customer</span> <br /> Segmentation</h3>
       </div>
-      <div>
+      <div className='text-8xl'>
         <i className="ri-arrow-right-up-line"></i>
       </div>
     </div>
