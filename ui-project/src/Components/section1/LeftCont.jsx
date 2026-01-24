664591a37b87bebd6be881f1ddfacf789363cc3d
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-24 20:28:27 +0530

    center->Left content done 2.0

--- ui-project/src/Components/section1/LeftCont.jsx
+++ ui-project/src/Components/section1/LeftCont.jsx
@@ -5,7 +5,7 @@ const leftContent = () => {
   return (
     <div className='h-full w-3/11 justify-between flex flex-col justify-between'>
       <div className='p-5'>
-        <h3 className='mb-7 text-5xl font-bold'>Prospective <br /> <span>Customer</span> <br /> Segmentation</h3>
+        <h3 className='mb-7 text-5xl font-bold leading-[1.1]'>Prospective <br /> <span className='text-gray-600'>customer</span> <br /> segmentation</h3>
         <p className='text-xl font-medium text-gray-700'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque rerum eligendi ducimus nostrum est tempora, voluptates voluptate maiores quae quas?</p>
       </div>
       <div className='text-9xl'>
