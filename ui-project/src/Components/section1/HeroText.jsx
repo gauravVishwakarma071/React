c2fdff0e41db6814ccfec61afc22e2d1a36e95c7
Gaurav <gauravvishwakarma071@gmail.com>
2026-01-26 09:25:18 +0530

    moduling LEFT-ConTent

---
+++ ui-project/src/Components/section1/HeroText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroText = () => {
+    return (
+
+        <div className='p-5'>
+            <h3 className='mb-7 text-5xl font-bold leading-[1.1]'>Prospective <br /> <span className='text-gray-600'>customer</span> <br /> segmentation</h3>
+            <p className='text-xl font-medium text-gray-700'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque rerum eligendi ducimus nostrum est tempora, voluptates voluptate maiores quae quas?</p>
+        </div>
+
+    )
+}
+
+export default HeroText
--- ui-project/src/Components/section1/LeftCont.jsx
+++ ui-project/src/Components/section1/LeftCont.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import 'remixicon/fonts/remixicon.css'
+import HeroText from './HeroText'
+import Arrow from './Arrow'
 
 const leftContent = () => {
   return (
     <div className='h-full w-3/11 justify-between flex flex-col justify-between'>
-      <div className='p-5'>
-        <h3 className='mb-7 text-5xl font-bold leading-[1.1]'>Prospective <br /> <span className='text-gray-600'>customer</span> <br /> segmentation</h3>
-        <p className='text-xl font-medium text-gray-700'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque rerum eligendi ducimus nostrum est tempora, voluptates voluptate maiores quae quas?</p>
-      </div>
-      <div className='text-9xl'>
-        <i className="ri-arrow-right-up-line"></i>
-      </div>
+      <HeroText />
+      <Arrow />
     </div>
   )
 }
